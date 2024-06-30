61d94fde822fa51e9e0bf7ba86c9374570aed62b
HarshProj <112640334+HarshProj@users.noreply.github.com>
2024-06-30 09:48:53 +0530

    Live projects added

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -5,6 +5,7 @@ import p1 from '../Assets/arczone.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFolder } from '@fortawesome/fontawesome-free-solid'
 import p2 from '../Assets/text.PNG'
+import { ArrowSquareOut } from "@phosphor-icons/react";
 export const Work = (props) => {
   const[des,setDes]=useState('desc');
   const[pdes,setPdes]=useState('project-desc');
@@ -65,7 +66,7 @@ else{
         <p>Express</p>
        </div>
        <div className="go-through">
-       <li><a href="https://github.com/HarshProj/Arc-Zone-client-side.git" target='_blanck' class="source-code"><img className='github' src={githublogo} loading="lazy" /></a>
+       <li><a href="https://arc-zone-client-side.onrender.com" target='_blanck' class="source-code"><ArrowSquareOut size={32} /></a>
        </li>
        <li><a href="https://github.com/HarshProj/Arc-Zone-client-side.git" target='_blanck' class="source-code"><img className='github' src={githublogo} loading="lazy" /></a></li>
 
@@ -92,7 +93,7 @@ else{
         <p>Express</p>
        </div>
        <div className="go-through go-through-p2">
-       <li><a href="https://github.com/HarshProj/inotebook.git" target='_blanck' class="source-code"><img className='github' src={githublogo} loading="lazy" /></a></li>
+       <li><a href="https://github.com/HarshProj/inotebook.git" target='_blanck' class="source-code"><ArrowSquareOut size={32} /></a></li>
        <li><a href="https://github.com/HarshProj/inotebook.git" target='_blanck' class="source-code"><img className='github' src={githublogo} loading="lazy" /></a></li>
 
        </div>
@@ -116,7 +117,7 @@ else{
       <div className="folder"><FontAwesomeIcon icon={faFolder} style={{width:'100%',height:"100%"}} className='fldr' /></div>
       
       <div className="folder-urls"><div id='mip' className="go-through mp">
-       <li><a href="https://www.linkedin.com/in/harsh-chauhan-0a34b01a0"  target='_blanck' class=""><img className='github'class="" src={githublogo} loading="lazy" /></a></li>
+       <li><a href="https://authentication-2h63.onrender.com"  target='_blanck' class=""><ArrowSquareOut size={32} /></a></li>
        <li><a href="https://www.linkedin.com/in/harsh-chauhan-0a34b01a0" target='_blanck' class=""><img className='github' src={githublogo} loading="lazy" /></a>
        </li>
 
@@ -142,8 +143,8 @@ else{
       <div className="folder"><FontAwesomeIcon icon={faFolder} style={{width:'100%',height:"100%"}} /></div>
       
       <div className="folder-urls"><div id='mip'  className="go-through mp">
+       <li><a href="https://www.linkedin.com/in/harsh-chauhan-0a34b01a0" target='_blanck' ><ArrowSquareOut size={32} /></a></li>
        <li><a href="https://github.com/HarshProj/Bharat-Intern-Task-II.git" target='_blanck' ><img className='github' src={githublogo} loading="lazy" /></a></li>
-       <li><a href="https://www.linkedin.com/in/harsh-chauhan-0a34b01a0" target='_blanck' ><img className='github' src={githublogo} loading="lazy" /></a></li>
 
        </div></div>
       </div>
@@ -167,7 +168,8 @@ else{
       <div className="folder"><FontAwesomeIcon icon={faFolder} style={{width:'100%',height:"100%"}} /></div>
       
       <div className="folder-urls"><div id='mip'  className="go-through mp">
-       <li><a href="https://github.com/HarshProj/FreeCode.git" target='_blanck' ><img className='github' src={githublogo} loading="lazy" /></a></li>
+       <li><a href="https://github.com/HarshProj/FreeCode.git" target='_blanck' ><ArrowSquareOut size={32} /></a></li>
+       
        <li><a href="https://github.com/HarshProj/FreeCode.git" target='_blanck' ><img className='github' src={githublogo} loading="lazy" /></a></li>
 
        </div></div>
@@ -191,8 +193,8 @@ else{
       <div className="folder"><FontAwesomeIcon icon={faFolder} style={{width:'100%',height:"100%"}} /></div>
       
       <div className="folder-urls"><div id='mip'  className="go-through mp">
-       <li><a href="https://github.com/HarshProj/FreeCode.git" target='_blanck' ><img className='github' src={githublogo} loading="lazy" /></a></li>
-       <li><a href="https://github.com/HarshProj/FreeCode.git" target='_blanck' ><img className='github' src={githublogo} loading="lazy" /></a></li>
+       <li><a href="https://github.com/HarshProj/News-app.git" target='_blanck' ><ArrowSquareOut size={32} /></a></li>
+       <li><a href="https://github.com/HarshProj/News-app.git" target='_blanck' ><img className='github' src={githublogo} loading="lazy" /></a></li>
 
        </div></div>
       </div>
